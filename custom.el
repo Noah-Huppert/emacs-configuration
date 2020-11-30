5d17b582dadad3a5aa4d899a520bdb57a68a75b2
Noah Huppert <contact@noahh.io>
2020-11-30 02:29:39 -0500

    uninstalled vterm Due to not using it

--- custom.el
+++ custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(projectile shell-switcher transpose-frame doom-themes vterm vlf tangotango-theme gitignore-mode protobuf-mode editorconfig rust-mode company-lsp lsp-mode company zenburn-theme web-narrow-mode tide typescript-mode dart-mode terraform-mode web-mode-edit-element vue-mode python-mode dockerfile-mode elixir-mode ## go-mode markdown-mode+ rainbow-mode popup paradox markdown-mode helm-core)))
+   '(projectile shell-switcher transpose-frame doom-themes vlf tangotango-theme gitignore-mode protobuf-mode editorconfig rust-mode company-lsp lsp-mode company zenburn-theme web-narrow-mode tide typescript-mode dart-mode terraform-mode web-mode-edit-element vue-mode python-mode dockerfile-mode elixir-mode ## go-mode markdown-mode+ rainbow-mode popup paradox markdown-mode helm-core))
+ '(projectile-mode t nil (projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
