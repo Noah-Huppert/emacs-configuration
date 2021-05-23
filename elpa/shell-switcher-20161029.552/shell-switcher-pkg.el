7305e5bc2e254f8ef1eb181076b797b86aa6eabd
Noah Huppert <contact@noahh.io>
2021-05-23 14:49:18 -0400

    updated elpa

--- elpa/shell-switcher-20161029.552/shell-switcher-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "shell-switcher" "20161029.552" "Provide fast switching between shell buffers."
-  '((emacs "24"))
-  :commit "28a7f753dd7addd2933510526f52620cb5a22048")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
