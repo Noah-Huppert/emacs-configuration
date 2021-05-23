19a608f5aa685f8679e11d7433de3640e2b16e30
Noah Huppert <contact@noahh.io>
2021-05-23 14:49:51 -0400

    +yaml mode customization

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dap-mode json-mode projectile shell-switcher transpose-frame doom-themes vlf tangotango-theme gitignore-mode protobuf-mode editorconfig rust-mode company-lsp lsp-mode company zenburn-theme web-narrow-mode tide typescript-mode dart-mode terraform-mode web-mode-edit-element vue-mode python-mode dockerfile-mode elixir-mode ## go-mode markdown-mode+ rainbow-mode popup paradox markdown-mode helm-core))
+   '(yaml-mode auto-complete dap-mode json-mode projectile shell-switcher transpose-frame doom-themes vlf tangotango-theme gitignore-mode protobuf-mode editorconfig rust-mode company-lsp lsp-mode company zenburn-theme web-narrow-mode tide typescript-mode dart-mode terraform-mode web-mode-edit-element vue-mode python-mode dockerfile-mode elixir-mode ## go-mode markdown-mode+ rainbow-mode popup paradox markdown-mode helm-core))
  '(projectile-mode t nil (projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
