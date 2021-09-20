f573b40480150dd0e67653bdf4627ad0245436e4
Noah Huppert <noahhuppert@admithub.com>
2021-09-20 12:44:54 -0400

    let customize do its thing
    First I fixed a syntax error where I had deleted part of the
    customization file manually. Then when I re-launched emacs customize
    added back the stuff I had deleted.
    
    I deleted this stuff because there was a merge conflict in the
    customization file and I wasn't sure which was correct. But I knew
    customize would just write whatever it needed if I deleted.

--- custom.el
+++ custom.el
@@ -6,6 +6,8 @@
  '(dired-hide-details-hide-information-lines t)
  '(image-dired-append-when-browsing t)
  '(image-dired-thumb-size 100)
+ '(package-selected-packages
+   '(markdown-mode gitignore-mode gitconfig-mode gitattributes-mode web-mode vterm use-package transpose-frame tide shell-switcher rainbow-mode px projectile org-bullets magit helm go-mode editorconfig doom-themes dired-subtree dired-avfs)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
