a431b5f82cf3e4be43df844fb7c4da3da2c2dd2b
Noah Huppert <noahhuppert@admithub.com>
2021-09-20 12:08:44 -0400

    updated customizations

--- custom.el
+++ custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(helm org-tempo magit vterm use-package sr-speedbar windresize org-bullets editorconfig yaml-mode auto-complete dap-mode json-mode projectile shell-switcher transpose-frame doom-themes vlf tangotango-theme gitignore-mode protobuf-mode rust-mode company-lsp lsp-mode company zenburn-theme web-narrow-mode tide typescript-mode dart-mode terraform-mode web-mode-edit-element vue-mode python-mode dockerfile-mode elixir-mode ## go-mode markdown-mode+ rainbow-mode popup paradox markdown-mode helm-core))
- '(projectile-mode t nil (projectile)))
+   '(web-mode doom-one helm org-tempo magit vterm use-package sr-speedbar windresize org-bullets editorconfig yaml-mode auto-complete dap-mode json-mode projectile shell-switcher transpose-frame doom-themes vlf tangotango-theme gitignore-mode protobuf-mode rust-mode company-lsp lsp-mode company zenburn-theme web-narrow-mode tide typescript-mode dart-mode terraform-mode web-mode-edit-element vue-mode python-mode dockerfile-mode elixir-mode ## go-mode markdown-mode+ rainbow-mode popup paradox markdown-mode helm-core)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
