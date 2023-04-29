7349650e53ef1dda1ec24dc8b60956554dc26e2c
Noah Huppert <contact@noahh.io>
2023-04-29 16:59:54 -0400

    magit: rm manual keybind
    I believe the default one not working was due to an init.el error
    above this pkg.

--- init.el
+++ init.el
@@ -173,7 +173,6 @@ current buffer."
               ("RET" . magit-diff-visit-file-other-window)
               :map magit-hunk-section-map
               ("RET" . magit-diff-visit-file-other-window))
-  :bind (("C-x g" . magit-status))
   :custom
   (ediff-window-setup-function 'ediff-setup-windows-plain) ; Make ediff navigation window not open in new window
   )
